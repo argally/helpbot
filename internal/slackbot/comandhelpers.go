cca400bc255e90a585ab6b679e98709e949457bf
argally <amandargalligan@gmail.com>
2025-03-24 13:15:35 +0000

    feat: add Slack bot manifest and update command response handling

--- internal/slackbot/comandhelpers.go
+++ internal/slackbot/comandhelpers.go
@@ -68,7 +68,7 @@ func AzureHelpers() *slacker.CommandDefinition {
 				return
 			}
 			attachments := createSlackAttachment(result)
-			ctx.Response().ReplyBlocks(attachments)
+			ctx.Response().PostBlocks(ctx.Event().ChannelID, attachments)
 		},
 	}
 	return azurebot
